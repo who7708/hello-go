238457ed716c3e7d9ba52ece168cda79003630df
Chris <who7708@163.com>
2023-08-18 12:40:14 +0800

    update log

--- test6/main.go
+++ test6/main.go
@@ -39,14 +39,17 @@ func main() {
 // 打印 murmur3 hash 后的字符串，与java一致
 func testMurmur3_2(str string) string {
 	b1 := []byte(str)
-
 	hasher := murmur3.New32()
 	hasher.Write(b1)
 	r := hasher.Sum([]byte{})
+	// for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+	// 	r[i], r[j] = r[j], r[i]
+	// }
 	r = reverse(r)
+
 	// fmt.Printf("r: %v\n", string(r))
 	s := hex.EncodeToString(r)
-	fmt.Printf("EncodeToString(b): %v\n", s)
+	fmt.Printf("murmur3(%s): %v\n", str, s)
 	return str
 }
 
