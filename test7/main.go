76e976873483a0bca849a988a6bea5c7bcd86a42
Chris <who7708@163.com>
2023-08-18 15:30:41 +0800

    测试go的线程安全，仍无法保证

--- test7/main.go
+++ test7/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var i int32 = 0
 
-// ++ 操作
+// ++ 操作, 非线程安全
 func doAdd() {
 	// i++ 非原子操作,所以在多线程下会不正确
 	i++
+}
+
+// ++ 操作
+func doAdd2(c chan int32) {
+	// i++ 非原子操作,所以在多线程下会不正确
+	i++
+	c <- i
 }
 
 // 耗时打印
@@ -36,10 +44,30 @@ func test1() {
 	fmt.Printf("i: %% , cost %%\n")
 }
 
-func main() {
+// defer 方式打印耗时
+func test2() {
+
 	defer timeCost(time.Now())
 	for i := 0; i < 50000; i++ {
 		go doAdd()
 	}
 	fmt.Printf("i: %v\n", i)
+}
+
+var (
+	rwlock sync.RWMutex
+)
+
+func main() {
+
+	ch := make(chan int32)
+	defer timeCost(time.Now())
+	for i := 0; i < 50000; i++ {
+		// 还是一样无法保证线程安全,为啥呢?
+		rwlock.TryLock()
+		go doAdd2(ch)
+		rwlock.Unlock()
+	}
+	fmt.Printf("i: %v\n", <-ch)
+
 }
