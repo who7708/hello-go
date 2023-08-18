aa75e3f1c8ca731702664db0f89407fbbf9a724c
Chris <who7708@163.com>
2023-08-18 18:23:21 +0800

    ws: 使用go实现websocket并在页面上建立连接发送消息和接收消息

--- websocket-go/go.mod
+++ websocket-go/go.mod
@@ -1,3 +1,5 @@
 module ws-go
 
 go 1.19
+
+require github.com/gorilla/websocket v1.5.0
--- websocket-go/main.go
+++ websocket-go/main.go
@@ -1,10 +1,55 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	// 写内容
-	w.Write([]byte("hello"))
+	// // 写内容
+	// w.Write([]byte("hello"))
+	fmt.Println("Upgrade 开始建立连接")
+
+	var (
+		conn *websocket.Conn
+		err  error
+		data []byte
+	)
+	// Upgrade: websocket
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		fmt.Println("Upgrade 异常")
+		return
+	}
+
+	//  websocket.conn
+	for {
+		//  Text, Binary
+		// 读取消息
+		if _, data, err = conn.ReadMessage(); err != nil {
+			fmt.Println("读取异常")
+			goto ERR
+		}
+		// 回写消息
+		if err = conn.WriteMessage(websocket.TextMessage, data); err != nil {
+			fmt.Println("写入消息")
+			goto ERR
+		}
+	}
+
+ERR:
+	conn.Close()
+
 }
 
 func main() {
