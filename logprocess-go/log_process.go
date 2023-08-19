670eb9e0cde5bfc3403a25d7fe213f2391316fc4
Chris <who7708@163.com>
2023-08-20 01:42:31 +0800

    日志监控相关模块优化

--- logprocess-go/log_process.go
+++ logprocess-go/log_process.go
@@ -6,21 +6,52 @@ import (
 	"time"
 )
 
+// 读取接口
+type Reader interface {
+	Read(rc chan string)
+}
+
+// 写入接口
+type Writer interface {
+	Write(wc chan string)
+}
+
 type LogProcess struct {
 	// channel
 	rc chan string
 	wc chan string
-	// 读取文件的路径
+	// // 读取文件的路径
+	// path string
+	// // influx data source
+	// influxDBDsn string
+	read  Reader
+	write Writer
+}
+
+type ReadFromFile struct {
+	// 文件路径
 	path string
+}
+
+type WriteToInfluxDB struct {
 	// influx data source
 	influxDBDsn string
 }
 
-// 读取模块
-func (lp *LogProcess) ReadFromFile() {
+// 实现读取接口
+func (r *ReadFromFile) Read(rc chan string) {
 	line := "message"
-	lp.rc <- line
+	rc <- line
 }
+
+// 实现写入接口
+func (w *WriteToInfluxDB) Write(wc chan string) {
+	fmt.Println(<-wc)
+}
+
+// // 读取模块
+// func (lp *LogProcess) ReadFromFile() {
+// }
 
 // 解析模块
 func (lp *LogProcess) Process() {
@@ -28,25 +59,35 @@ func (lp *LogProcess) Process() {
 	lp.wc <- strings.ToUpper(data)
 }
 
-// 写入模块
-func (lp *LogProcess) WriteToInfluxDB() {
-	fmt.Println(<-lp.wc)
-}
+// // 写入模块
+// func (lp *LogProcess) WriteToInfluxDB() {
+// 	fmt.Println(<-lp.wc)
+// }
 
 func main() {
+	r := &ReadFromFile{
+		path: "/tmp/access.log",
+	}
+
+	w := &WriteToInfluxDB{
+		influxDBDsn: "username&password...",
+	}
+
 	lp := &LogProcess{
 		// 初始化
-		rc:          make(chan string),
-		wc:          make(chan string),
-		path:        "/tmp/access.log",
-		influxDBDsn: "username&password...",
+		rc: make(chan string),
+		wc: make(chan string),
+		// path:        "/tmp/access.log",
+		// influxDBDsn: "username&password...",
+		read:  r,
+		write: w,
 	}
 
 	// 并发执行
 	// go (*lp).ReadFromFile()
-	go lp.ReadFromFile()
+	go lp.read.Read(lp.rc)
 	go lp.Process()
-	go lp.WriteToInfluxDB()
+	go lp.write.Write(lp.wc)
 
 	time.Sleep(1 * time.Second)
 }
