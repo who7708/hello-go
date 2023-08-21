aa6c7e0a22d16fc5b9f40fdbcb5bd3df3de03b7b
Chris <who7708@163.com>
2023-08-21 12:57:02 +0800

    归并结点事例

--- channel-sample/cmd/piplinedemo/main.go
+++ channel-sample/cmd/piplinedemo/main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	p := pipline.InMemSort(
-		pipline.ArraySource(3, 2, 6, 7, 4),
-	)
 
+	// p := pipline.InMemSort(
+	// 	pipline.ArraySource(3, 2, 6, 7, 4),
+	// )
+	p := pipline.Merge(
+		pipline.InMemSort(
+			pipline.ArraySource(3, 2, 6, 7, 4),
+		),
+		pipline.InMemSort(
+			pipline.ArraySource(7, 4, 0, 3, 2, 13, 8),
+		))
 	// for {
 	// 	if num, ok := <-p; ok {
 	// 		fmt.Println(num)
